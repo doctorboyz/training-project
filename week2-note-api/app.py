66ea1aadbb08ea8aa3da5b8076a97f4b04908d1c
doctorboyz <150646635+doctorboyz@users.noreply.github.com>
2025-04-17 10:38:32 +0700

    Update app.py

--- week2-note-api/app.py
+++ week2-note-api/app.py
@@ -55,4 +55,5 @@ def get_single_note(note_id):
     return jsonify({"error": "Note not found"}), 404
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
